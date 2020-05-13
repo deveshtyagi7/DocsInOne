5256c47d9051f312cd7d34079bcaf02e32baa94a
Devesh <Tyagi77@outlook.vom>
2020-05-13 15:57:20 +0530

    Initial Commit

---
+++ DocsInOne/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DocsInOne
+//
+//  Created by Devesh Tyagi on 13/05/20.
+//  Copyright © 2020 deveshtyagi7. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
